ba2d82d346b58233bd7786d3bb520ef2b2a02922
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-08-27 00:02:16 +0500

    Add files via upload

---
+++ Python Code + SS/Basics 1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # # # Basics of Python
+
+# In[1]:
+
+
+#Python 3
+print("Basics of python")
+
+
+# In[2]:
+
+
+name=input("Enter Your Name: ")
+print("My Name is ", name)
+print("Data tye of Variable name is: ")
+type(name) #To check datatype of a variable
+
+
+# In[3]:
+
+
+num1,num2, num3=12,2.4,'wicky'
+print(num1)
+print(num2)
+print(num3)
+
+
+# In[5]:
+
+
+numbers=[12,2.4,'wicky']
+print(numbers)
+print(numbers[2])
+
+
+# In[ ]:
+
+
+
+
