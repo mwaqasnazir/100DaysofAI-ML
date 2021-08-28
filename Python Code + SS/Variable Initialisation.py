2a323c4462853f11d7b1a356921494d49dde2294
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-08-28 20:22:25 +0500

    Statements & Variable Initilisation in Python

---
+++ Python Code + SS/Variable Initialisation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Direct Initialisation Method
+
+# In[3]:
+
+
+# initialising variable directly
+a = 5
+ 
+# printing value of a
+print ("The value of a is: " + str(a))  #here i am using str for concatination a int variable in string statment
+
+
+# ### Using Conditional Operator (?:)
+
+# In[6]:
+
+
+# Initialising variables using Conditional Operator
+b = 15 if 20 > 10 else 0
+print("The value of b is: " , str(b))
+
+
+# ### One liner if-else instead of Conditional Operator
+
+# In[7]:
+
+
+# initialising variable using one liner if-else
+c = 12 if 20 > 10 else 0
+print ("The value of c is: " + str(a))
+
+
+# In[ ]:
+
+
+
+
