aa202347d9964ee685cab11764a45921ac73cd2f
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-08-31 22:02:26 +0500

    Strings Lists & Tuples

---
+++ Python Code + SS/Strings Lists & Tuples.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Python Data Types
+
+# In[1]:
+
+
+# String Data Type 
+# Assigning string to a variable
+a = "Hi! It's me Wicky"
+print (a)
+
+
+# In[5]:
+
+
+# Declaring a list
+L = [1, "Mr. " , "Wicky" , 1+2]
+print (L)
+L.append(6) # To add new members to list
+print (L)
+L.pop()    # To remove members of list
+print (L)
+print (L[2])
+
+
+# In[8]:
+
+
+#Declearing a Tuple
+tup = (1, "Mr. " , "Wicky" , 1+2)
+print(tup)
+print(tup[1] + tup[2])
+
+
+# In[ ]:
+
+
+
+
