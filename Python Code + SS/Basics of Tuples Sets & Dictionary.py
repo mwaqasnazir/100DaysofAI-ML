3bc16e12ffe28f92ef362bafb37b80c7aa7b87a1
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-09-04 23:27:26 +0500

    Basics of Tuples Sets & Dictionary in Python

---
+++ Python Code + SS/Basics of Tuples Sets & Dictionary.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Basics of Tuples
+
+# In[1]:
+
+
+#Creating an empty Tuple
+Tuple1 = ()
+print("Initial empty Tuple: ")
+print (Tuple1)
+ 
+# Creating a Tuple with
+# the use of list
+Tuple2 = [1, 2, 4, 5, 6]
+print("\nTuple using List: ")
+print(Tuple2)
+
+
+# ## Basics of Sets
+
+# In[4]:
+
+
+# Creating a Set
+set1 = set()
+print("Initial blank Set: ")
+print(set1)
+ 
+
+set1 = set([1, 2, 4, 4, 3, 3, 3, 6, 5])
+print("\nSet with the use of Numbers: ")
+print(set1)
+
+
+# ## Basics of Dictionary
+
+# In[5]:
+
+
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+ 
+# Creating a Dictionary
+# with dict() method
+Dict = dict({1: 'Do', 2: 'or', 3:'Die'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+
+# In[ ]:
+
+
+
+
