0812f2065fbebe76dd97ef4501adb047925a726c
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-09-08 22:55:24 +0500

    Custom Module File

---
+++ Python Code + SS/Module.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Simple Modules 
+
+def add(x, y):
+    print (x+y)
+    
+def subtract(x, y):
+    print (x-y)
+
+def divide(x, y):
+    print (x/y)
+
+def multiply(x, y):
+    print (x*y)
+
+
+# In[ ]:
+
+
+
+
