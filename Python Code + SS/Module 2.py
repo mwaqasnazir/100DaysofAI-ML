55f5ee821224202a34210ecdf04792880694bcc5
Muhammad Waqas Nazir <75742953+mwaqasnazir@users.noreply.github.com>
2021-09-11 22:36:34 +0500

    Module & Module Nootebook

---
+++ Python Code + SS/Module 2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Simple Modules 
+
+def add(x, y):
+    print (x+y)
+    
+def subtract(x, y):
+    print (x-y)
+
+def divide(x, y):
+    print (x/y)
+
+def multiply(x, y):
+    print (x*y)
+    
+    add(5,6)
+    subtract(6,3)
+    divide(8,2)
+    multiply(2,2)
+
+    
